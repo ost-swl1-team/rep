4fabce2cf43ef686a83754180eea5ce5dfd825dc
Christoph Baumgarten <christoph.baumgarten@fhsg.ch>
2019-09-12 15:30:59 +0200

    Third commit

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ http.createServer(
     function (request, response) {    
         // Send the HTTP header
         // HTTP Status: 200 : OK
-        // Content Type: text/plai
+        // Content Type: text/plain
         response.writeHead(200, {'Content-Type': 'text/plain'});
 
         // Send the response body as "Hello World"
