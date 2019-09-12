7f07f012ff52b44d46a4e19ceae3f9b3aa1740b3
Christoph Baumgarten <christoph.baumgarten@fhsg.ch>
2019-09-12 12:45:29 +0200

    second commit

--- test.js
+++ test.js
@@ -12,4 +12,4 @@ http.createServer(
 }).listen(8081);
 
 // Console will print the message in the terminal
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8081/'); 
