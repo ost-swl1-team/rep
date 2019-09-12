c4c2710fd6c146e26e75fed158464ec2e3685212
Christoph Baumgarten <christoph.baumgarten@fhsg.ch>
2019-09-12 16:21:40 +0200

    Changes under test2

--- test.js
+++ test.js
@@ -7,8 +7,8 @@ http.createServer(
         // Content Type: text/plain
         response.writeHead(200, {'Content-Type': 'text/plain'});
 
-        // Send the response body as "Hello World"
-        response.end('Hello World\n');
+        // Send the response body as "Hi there"
+        response.end('Hi there\n');
 }).listen(8081);
 
 // Console will print the message in the terminal
