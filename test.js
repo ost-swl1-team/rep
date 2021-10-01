6f8b2d782fdf21184c6d67e1099fe5fe99d863a2
ChrisBgt <christoph.baumgarten@ost.ch>
2021-10-01 16:02:24 +0200

    Story "Start Screen" Fixes #2

--- test.js
+++ test.js
@@ -4,12 +4,12 @@ http.createServer(
     function (request, response) {    
         // Send the HTTP header
         // HTTP Status: 200 : OK
-        // Content Type: text/plain
+        // Content Type: text/plai
         response.writeHead(200, {'Content-Type': 'text/plain'});
 
         // Send the response body as "Hello World"
-        response.end('Hello World :-) test10 changed\n');
+        response.end('This is a new start screen text ...\n');
 }).listen(8081);
 
 // Console will print the message in the terminal
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8081/'); 
